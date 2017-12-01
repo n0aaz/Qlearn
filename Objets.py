3081b29fa80a85e87d6fc41f914f416b864c74a2
n0aaz <aazdu03@gmail.com>
2017-12-01 16:47:04 +0100

    Create Objets.py

---
+++ Objets.py
@@ -0,0 +1,34 @@
+class Bloc:
+  def __init__(self):
+    self.couleur='blanc'
+    self.x=0
+    self.y=0
+    self.taille=[1,1]
+    self.prop='blanc'
+  
+class Obstacle(Bloc):
+  def __init__(self):
+    Bloc.__init__(self)
+    self.couleur='noir'
+    self.prop= 'obstacle'
+  
+class Recompense(Bloc):
+  def __init__(self):
+    Bloc.__init__(self)
+    self.couleur='bleu'
+    self.bonus=1
+    self.prop='recompense'
+    self.passage=False
+    
+    #Exemple pour johann 
+    ### def deplacement(dx,dy):
+    blocencours=matrice[bloc.x+dx,bloc]
+    if matrice[bloc.x+dx,bloc] !=0:
+      if blocencours.prop== 'obstacle' :
+        pass
+      elif blocencours.prop == 'recompense':
+        self.score+=blocencours.bonus
+        blocencours.passage=True
+    else:
+      self.x=self.x+dx
+        ###
