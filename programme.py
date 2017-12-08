9a33b6f94ebc4a50fa99c6f38ca5d84a3e9959f0
n0aaz <aazdu03@gmail.com>
2017-12-08 16:41:24 +0100

    Update programme.py

--- programme.py
+++ programme.py
@@ -3,16 +3,21 @@ from Objets import *
 
 fenetre = Tk()
 
+
 label = Label(fenetre, text="Qlearning")
 label.pack()
 
-fenetre.mainloop()
 
 large,haut=1000,1000
 taille=[large,haut]
 bloclarge,blochaut=10,10
 matrice=[bloclarge,blochaut]
 blocs=[]
+
+canvas = Canvas(fenetre, width=large, height=haut, background='white')
+
+j=Joueur()
+
 
 def generfond():
   for i in range(matrice[0]):
@@ -21,12 +26,15 @@ def generfond():
         a=Obstacle()
         a.x=i*(large/bloclarge)
         a.y=i*(haut/blochaut)
-        a.genere(fenetre)
+        a.genere(canvas)
       else:
         a=Bloc()
         a.x=i*(large/bloclarge)
         a.y=i*(haut/blochaut)
-        a.genere(fenetre)
+        a.genere(canvas)
+        
+
       
 
+fenetre.mainloop()
 
