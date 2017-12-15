c366bff0038425551502faa41c95a352e579f7e6
N0aaz <aazdu03@gmail.com>
2017-12-15 16:49:37 +0100

    grille fonctionnelle

--- Objets.py
+++ Objets.py
@@ -1,15 +1,21 @@
+from tkinter import *
+
 class Bloc:
   def __init__(self):
     self.couleur="white"
     self.x=0
     self.y=0
-    self.taille=[1,1]
+    self.taille=[10,10]
     self.prop='blanc'
     self.bonus=-0.1
     
-  def generer(canvas):
-    canvas.create_rectangle(self.x*large, self.y*haut, (self.x +1)*large, (self.y+1)*haut, fill=self.couleur, width=1)
-  
+  def genere(self,canvas):
+    #print(self.x,self.y,self.taille)
+    a,b,c,d=self.x, self.y, (self.x)+self.taille[0], (self.y)+self.taille[1]
+    print(a,b,c,d)
+    truc=canvas.create_rectangle(a,b,c,d, fill=self.couleur, width=3) 
+    return truc
+    
 class Obstacle(Bloc):
   def __init__(self):
     super().__init__()
@@ -24,27 +30,23 @@ class Recompense(Bloc):
     self.prop='recompense'
     
     
-class Joueur(Bloc):   #joueur est il considéré comme un bloc comme les autres ? superposition possible ?
+class Joueur(Bloc):  
   def __init__(self):
     super().__init__()
-    self.couleur='vert'
+    self.couleur='green'
     self.score=0
     
-  def deplacement (dx,dy):
-    if matrice[self.x+dx][self.y+dy].prop='obstacle':   #devrait on créer la map avec des obstacles tout autour pour ne pas avoir de problèmes de limites ?
+  def deplacement (matrice,dx,dy):
+    if matrice[self.x+dx][self.y+dy].prop=='obstacle':   
       return
     self.x+=dx
     self.y+=dy
-    self.score+=matrice[self.x][self.y].bonus  #est ce que  ça écrase le bloc sous le joueur ? bonus de la case de départ ou  d'arrivée ?
-    if matrice[self.x][self.y].prop='recompense':
-      Recompense.prop='blanc'  #ça change les propriétés de la classe ou juste de ce bloc ?
+    self.score+=matrice[self.x][self.y].bonus  
+    if matrice[self.x][self.y].prop=='recompense':
+      Recompense.prop='white'
       Recompense.bonus=-0.1
-      Recompense.couleur='blanc'
-      #autre proposition
-      matrice[self.x][self.y].prop='blanc'
+      Recompense.couleur='white'
+      matrice[self.x][self.y].prop='white'
       matrice[self.x][self.y].bonus=-0.1
-      matrice[self.x][self.y].couleur='blanc'
+      matrice[self.x][self.y].couleur='white'
     
-        
-    
-   ###tkinter : module graphique (à regarder)
--- programme.py
+++ programme.py
@@ -1,5 +1,6 @@
 from tkinter import * 
 from Objets import *
+import numpy as np
 
 fenetre = Tk()
 
@@ -8,35 +9,38 @@ label = Label(fenetre, text="Qlearning")
 label.pack()
 
 
-large,haut=1000,1000
+large,haut=500,500
 
 bloclarge,blochaut=10,10
 
-matrice=[[None for i in range bloclarge]for j in range blochaut]
+matrice=[[[] for i in range(bloclarge)]for j in range(blochaut)]
 
 canvas = Canvas(fenetre, width=large, height=haut, background='white')
 
 joueur=Joueur()
 
 
-def generfond():
-  for i in range(bloclarge):
-    for j in range(blochaut):
-      if i==1 or j==1 or i==bloclarge-1 or i==blochaut-1:
+def generfond(canvas):
+  for i in range(0,bloclarge):
+    for j in range(0,blochaut):
+      if i==0 or j==0 or i==bloclarge-1 or j==blochaut-1: 
         a=Obstacle()
-        a.x=i*(large/bloclarge)
-        a.y=i*(haut/blochaut)
+        a.taille=(large/bloclarge),(haut/blochaut)
+        a.x,a.y=i*a.taille[0],j*a.taille[0]
         a.genere(canvas)
       else:
         a=Bloc()
-        a.x=i*(large/bloclarge)
-        a.y=i*(haut/blochaut)
+        a.taille=(large/bloclarge),(haut/blochaut)
+        a.x,a.y=i*a.taille[0],j*a.taille[0]
         a.genere(canvas)
         
-      matrice[i][j].append(a)
-       
+      matrice[i][j].append(int(a.prop=='obstacle'))
 
-      
-
+generfond(canvas)
+canvas.pack()
+joueur=Joueur()
+joueur.taille=(large/bloclarge),(haut/blochaut)
+j=joueur.genere(canvas)
 fenetre.mainloop()
 
+print(np.asarray(matrice))
