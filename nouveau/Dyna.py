6dfa90e099c846a51a5983bbb47da922447e1145
Johann-Cochet <34166099+Johann-Cochet@users.noreply.github.com>
2018-03-09 00:30:31 +0100

    Create Dyna.py

---
+++ nouveau/Dyna.py
@@ -0,0 +1,33 @@
+import World
+import threading
+import time
+import matplotlib.pyplot as plt
+from World import listescores
+
+#4 étapes : 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+World.start_game()
+	
+print(cpteur,listescores)
+plt.close()
+plt.plot(range(cpteur),listescores)
+plt.show()
