e5207c1df3a264dab50980fd9f0f1df57d70c62b
Johann-Cochet <34166099+Johann-Cochet@users.noreply.github.com>
2018-03-09 16:06:43 +0100

    Update Dyna.py

--- nouveau/Dyna.py
+++ nouveau/Dyna.py
@@ -4,25 +4,114 @@ import time
 import matplotlib.pyplot as plt
 from World import listescores
 
-#4 étapes : 
+discount = 0.9
+actions = World.actions
+states = []
+Q = {}
+R = {}
+cpteur=0
+stop=False
+
+#on initialise tous les états possibles , soit tous les tuples (a,b) où a,b appartiennent à [|0;x|]*[|0;y|]
+for i in range(World.x):
+    for j in range(World.y):
+        states.append((i, j))
 
 
+for state in states:
+    temp = {}
+    recomp = {}
+    for action in actions:
+        temp[action] = 0.1
+	recomp[action] = 0
+    Q[state] = temp
+    R[state] = recomp
+
+for (i, j, c, w) in World.specials:
+    for action in actions:
+        Q[(i, j)][action] = w
+	R[(i, j)][action] = w 
+
+print(Q)
+
+#cette fonction nous permet d'envoyer un signal d'arret au programme principal quand le score a convergé
+#on considère qu'il y a convergence lorsqu'un même score se répète 10 fois
+def signal_arret(liste):
+	n=len(liste)
+	if n<11:
+		return False
+	aux= liste[n-1]
+	
+	for k in range(n-2,n-10,-1):
+		if liste[k] != aux:
+			return False
+		aux = liste[k]
+	return True
+
+def do_action(action):
+    s = World.player
+    r = -World.score
+    
+    if action == actions[0]:
+        World.try_move(0, -1)
+    elif action == actions[1]:
+        World.try_move(0, 1)
+    elif action == actions[2]:
+        World.try_move(-1, 0)
+    elif action == actions[3]:
+        World.try_move(1, 0)
+    else:
+        return
+    s2 = World.player
+    r += World.score
+    return s, action, r, s2
 
 
+def max_Q(s):
+    val ,act= 0,0
+    for a, q in Q[s].items():
+        if val == 0 or (q > val):
+            val , act = q,a
+    return act, val
 
 
+def inc_Q(s, a, alpha, inc):
+    Q[s][a] = alpha*(Q[s][a] + inc)
 
 
+def run():
+    global discount,cpteur,listescores
+    time.sleep(1)
+    alpha = 0.5
+    t = 1
+    while not signal_arret(listescores):
+        # Choix de l'action menant à la meilleure récompense
+        s = World.player
+        max_act, max_val = max_Q(s)
+        (s, a, r, s2) = do_action(max_act)
+
+        # Modification de la matrice Q
+        max_act, max_val = max_Q(s2)
+        inc_Q(s, a, alpha, r + discount * max_val)
+
+        # Check if the game has restarted
+        t += 1.0
+        if World.has_restarted():
+            World.restart_game()
+            cpteur += 1
+            time.sleep(0.001)
+            t = 1.0
+
+        # vitesse de rafraichissement de l'interface graphique
+        time.sleep(0.00001)
+    #World.master.destroy()
 
 
-
-
-
-
-
-
-
-
+t = threading.Thread(target=run)
+t.daemon = True
+t.start()
+#if signal_arret(listescores):
+#t.join()
 
 
 World.start_game()
